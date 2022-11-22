915ad8656c2899b47304f261c182ddd6e1d96adb
brennan-lee <113056160+brennan-lee@users.noreply.github.com>
2022-11-22 18:30:53 -0500

    age requidments

--- js/script.js
+++ js/script.js
@@ -10,8 +10,8 @@
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
-    scope: "/ICS2O-PWA-Test/",
+  navigator.serviceWorker.register("/ICS2O-5-03ICS2O-5-03ICS2O-5-03/sw.js", {
+    scope: "/ICS2O-5-03/",
   })
 }
 
@@ -20,4 +20,18 @@ if (navigator.serviceWorker) {
  */
 function myButtonClicked() {
   document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+}
+
+function myButtonClicked() {
+  buttonOnChecked = document.getElementById("flash1").checked
+
+  if (age >= 17) {
+    console.log ("you can see an R movie alone")
+  } else if(age >= 13) {
+      console.log("you can see a PG-13 movie alone")
+  } else if(age >= 5) {
+  console.log("you can see a g or PG movie alone")
+  } else {
+  console.log("your too young for most things")
+  }
 }
