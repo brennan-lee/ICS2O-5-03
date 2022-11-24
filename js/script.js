a2a0e37597b7b0fd936242b0dbb96e6ed35ccbf1
brennan-lee <113056160+brennan-lee@users.noreply.github.com>
2022-11-24 14:57:45 -0500

    title

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2022 Brennan Lee All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Brennan Lee
+// Created on: Nov 2022
 // This file contains the JS functions for index.html
 
 "use strict"
@@ -10,7 +10,7 @@
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-5-03ICS2O-5-03ICS2O-5-03/sw.js", {
+  navigator.serviceWorker.register("/ICS2O-5-03/sw.js", {
     scope: "/ICS2O-5-03/",
   })
 }
